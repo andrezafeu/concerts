0812d8977adf72c5687dd23da2b7c934b7e5f39b
Andreza Feu <andrezafeu@gmail.com>
2016-07-27 16:50:27 -0400

    Create action and views for adding new concert

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -1,2 +1,17 @@
 class ConcertsController < ApplicationController
+	def new
+		@my_concert = Concert.new
+	end
+	def create
+		@my_concert = Concert.new(
+			:artist => params[:concert][:artist],
+			:venue => params[:concert][:venue],
+			:city => params[:concert][:city],
+			:date => params[:concert][:date],
+			:price => params[:concert][:price],
+			:description => params[:concert][:description]
+			)
+		@my_concert.save
+		redirect_to "/concerts/#{@my_concert.id}"
+	end
 end
